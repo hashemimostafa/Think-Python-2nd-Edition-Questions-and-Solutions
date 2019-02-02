a8b3836992054f7f3b303b98b1905c3f57915812
Hashemi <38297179+hashemimostafa@users.noreply.github.com>
2019-02-02 18:56:25 +0330

    Add files via upload

---
+++ 8.2.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# There is a string method called count that is similar to the function in 
+# Section 8.7.
+# Read the documentation of this method and write an invocation that 
+# counts the number of a’s in 'banana'.
+# =============================================================================
+
+s = 'banana'
+w = s.count('a')
+print(w)
